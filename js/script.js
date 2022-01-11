e7cafd7d5c1437a235ac527b5a7c5b2df36dfe30
Sara Romão <sara-romao@live.com>
2022-01-10 23:25:09 -0300

    parametro url

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ function ativarLink(link) {
 }
 links.forEach(ativarLink)
 
+//Ativar Itens do orçamento
+
+
+const parametros = new URLSearchParams(location.search);
+
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true;
+    }
+   
+ 
+}
+
+parametros.forEach(ativarProduto)
+
+
+
