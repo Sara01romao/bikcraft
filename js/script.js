6f940f7e17b233b7b1ff6a2af1bff504fac88bc6
Sara Romão <sara-romao@live.com>
2022-01-11 11:15:56 -0300

    controle de mostrar respotas

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ function ativarLink(link) {
     const href = link.href;
 
     if (url.includes(href)) {
-        console.log(link)
         link.classList.add("ativo")
     }
 
@@ -32,4 +31,28 @@ function ativarProduto(parametro){
 parametros.forEach(ativarProduto)
 
 
+//Peguntas frequentes
 
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+    
+
+
+   
+    resposta.classList.toggle("ativa")
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+
+    
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
